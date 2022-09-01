4ef091eed307c13194fbdacc131e992b24ff841f
Felipe <felsborges@gmail.com>
2022-09-01 20:11:52 -0300

    Retorno do banco de dados

--- cia_animal_api.py
+++ cia_animal_api.py
@@ -16,13 +16,35 @@ app = Flask(__name__)
 def produtos(): # View Function
     # O primeiro passo para o uso do banco de dados é a conexão
     # Para estabelecer uma conexão é que seja informado o caminho da conexão
-    #   este caminho é informado através de um texto
+    #   este caminho é informado através de um texto, infornando o nome do arquivo
     con = sqlite3.connect('cia-animal.db')
 
-    dic = {
-        'Chave1': 'Valor de teste'
-    }
+    # Por padrão o retorno dos dados é uma lista de tuplas, mas se desejamos uma lista
+    #   de dicionário o sqlite3 possuiu uma configuração na conexão para que esse retorno
+    #   seja feito
+    # con.row_factory = sqlite3.Row
+
+    # Após a conexão é necessário estabeler um caminho de comunicação
+    #   este caminho será chamdo de cursor, nele poderemos enviar comandos ao banco.
+    # Os comando do banco de dados utilizaram a liguagem SQL
+    cur = con.cursor()
+
+    # Como boa prática, é interessante criar uma variável texto contendo a instrução SQL
+    sql = 'SELECT nome FROM produtos'
+
+    # A variável texto criada no passo anterior é passada por parâmetro para o método 
+    #   execute é simplesmente executa o comando SQL
+    cur.execute( sql )
+
+    # Para os casos de comandos SELECT, o banco de dados deixa disponível o resultado,
+    #   no entanto, este resultado deve ser coletado através do comando
+    #   de bucar tudo (fetchall), e após será armazenado em uma variável qualquer.
+    dados = cur.fetchall()
+
+    # Quando não é mais necessário a utilização do banco de dados, é importante que ele
+    #   seja encerrado através do comando close
+    con.close()
 
     # Para que esta função retorne uma API é necessário que seja retornado um tipo de coleção
     #   podendo ser uma lista ou um dicionário
-    return dic
+    return dados
